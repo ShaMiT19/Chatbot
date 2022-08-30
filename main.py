480df8c4a6acfeb4cb999c4accd507cf93d41e8e
Shamit Savant <shamitsavant1919@gmail.com>
2022-08-31 00:19:13 +0530

    Final program file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import nltk
 # nltk.download('all')                             #if some nltk error occurs
 from nltk.stem.lancaster import LancasterStemmer
@@ -7,58 +8,66 @@ import numpy
 import tflearn
 import tensorflow
 import random
+import pickle
 
 import json
 with open('intents.json') as file:
     data = json.load(file)
 
-words = []
-labels = []
-docs_x = []
-docs_y = []
+try: 
+    with open("data.pickle", "rb") as f:
+        words, labels, training, output = pickle.load(f) 
 
-                                                    #stemming the words -> basically taking the words to their root
+except:
+    words = []
+    labels = []
+    docs_x = []
+    docs_y = []
 
-for intent in data["intents"]:
-    for pattern in intent["patterns"]:
-        wrds = nltk.word_tokenize(pattern)          #tokenize just gets us all the words from the patterns -> returns a list
-        words.extend(wrds)                          #adding the words from pattern or rather extending wrds list into empty words list. 
-        docs_x.append(wrds)                         #adding the words from pattern to doc.x
-        docs_y.append(intent["tag"])                #adding the words form tag to doc.y
-        
-    if intent['tag'] not in labels:
-        labels.append(intent['tag'])                #adding the tag uniquely to label list.
+                                                        #stemming the words -> basically taking the words to their root
 
-words = [stemmer.stem(w.lower()) for w in words if w != "?"]    #adding stemmed words to words list
-words = sorted(list(set(words)))                    #set removes all the duplicates, we convert it back to list and then its sorted.
+    for intent in data["intents"]:
+        for pattern in intent["patterns"]:
+            wrds = nltk.word_tokenize(pattern)          #tokenize just gets us all the words from the patterns -> returns a list
+            words.extend(wrds)                          #adding the words from pattern or rather extending wrds list into empty words list. 
+            docs_x.append(wrds)                         #adding the words from pattern to doc.x
+            docs_y.append(intent["tag"])                #adding the words form tag to doc.y
+            
+        if intent['tag'] not in labels:
+            labels.append(intent['tag'])                #adding the tag uniquely to label list.
 
-labels = sorted(labels)                             #setting the labels as well.
+    words = [stemmer.stem(w.lower()) for w in words if w != "?"]    #adding stemmed words to words list
+    words = sorted(list(set(words)))                    #set removes all the duplicates, we convert it back to list and then its sorted.
 
-training = []
-output = []
+    labels = sorted(labels)                             #setting the labels as well.
 
-out_empty = [0 for _ in range(len(labels))]         #Adding zeros for each tags.
+    training = []
+    output = []
 
-for x, doc in enumerate(docs_x):
-    bag = []
+    out_empty = [0 for _ in range(len(labels))]         #Adding zeros for each tags.
 
-    wrds = [stemmer.stem(w.lower()) for w in doc]
+    for x, doc in enumerate(docs_x):
+        bag = []
 
-    for w in words:
-        if w in wrds:
-            bag.append(1)
-        else:
-            bag.append(0)
+        wrds = [stemmer.stem(w.lower()) for w in doc]
 
-    output_row = out_empty[:]
-    output_row[labels.index(docs_y[x])] = 1
+        for w in words:
+            if w in wrds:
+                bag.append(1)
+            else:
+                bag.append(0)
 
-    training.append(bag)
-    output.append(output_row)
+        output_row = out_empty[:]
+        output_row[labels.index(docs_y[x])] = 1
 
-training = numpy.array(training)                    #we need to convert them to numpy arrays, because we need them for usign tflearn
-output = numpy.array(output)                        #we need to convert them to numpy arrays, because we need them for usign tflearn
+        training.append(bag)
+        output.append(output_row)
 
+    training = numpy.array(training)                    #we need to convert them to numpy arrays, because we need them for usign tflearn
+    output = numpy.array(output)                        #we need to convert them to numpy arrays, because we need them for usign tflearn
+
+    with open("data.pickle", "wb") as f:                #This is for writing the 4 lists to the data.pickle file. So that we won't need to preprocess them again and again.
+        pickle.dump((words, labels, training, output), f)
 
 tensorflow.compat.v1.reset_default_graph()                   #resets all the underlying settings of the tensorflow. Don't really need to focus on this.
 
@@ -70,5 +79,47 @@ net = tflearn.regression(net)
 
 model = tflearn.DNN(net)
 
-model.fit(training, output, n_epoch=500, batch_size=8, show_metric=True)
-model.save("model.tflearn")
+try:
+    model.load("model.tflearn")
+except:
+    model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+    model.save("model.tflearn")
+
+# model.fit(training, output, n_epoch=500, batch_size=8, show_metric=True)
+# model.save("model.tflearn")
+
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
+
+    for se in s_words:
+        for i, w in enumerate(words):
+            if w == se:
+                bag[i] = 1
+            
+    return numpy.array(bag)
+
+
+def chat():
+    print("Start talking with the bot (type quit to stop)!")
+    while True:
+        inp = input("You: ")
+        if inp.lower() == "quit":
+            break
+
+        results = model.predict([bag_of_words(inp, words)])[0]
+        results_index = numpy.argmax(results)
+        tag = labels[results_index]
+
+        if results[results_index] > 0.8:
+            for tg in data["intents"]:
+                if tg['tag'] == tag:
+                    responses = tg['responses']
+
+            print(random.choice(responses))
+        else:
+            print("Sorry, but I did not get what you are trying to say")
+
+chat()
